4fe20666bec14b2607f4b005f0c4a7c292623fb2
Fero-Fero <lightning1227@hotmail.com>
2026-09-01 12:31:45 +0200

    ensure wotlk pre-patch is correctly interpreted

--- Wotlk/3.0 Naxxramas, Eye of Eternity, Obsidian Sanctum/sql/world/recreate_taxi_paths.sql
+++ Wotlk/3.0 Naxxramas, Eye of Eternity, Obsidian Sanctum/sql/world/recreate_taxi_paths.sql
@@ -1,7 +1,5 @@
 -- gameobject_template
 INSERT INTO acore_world.gameobject_template (entry, `type`, displayId, name, IconName, castBarCaption, unk1, `size`, Data0, Data1, Data2, Data3, Data4, Data5, Data6, Data7, Data8, Data9, Data10, Data11, Data12, Data13, Data14, Data15, Data16, Data17, Data18, Data19, Data20, Data21, Data22, Data23, AIName, ScriptName, VerifiedBuild) VALUES
-(177233, 15, 7087, 'Ship, Night Elf (Feathermoon Ferry)', '', '', '', 1.0, 777, 30, 1, 0, 0, 1, 587, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 12340),
-(181646, 15, 7087, 'Ship, Night Elf (Elune''s Blessing)', '', '', '', 1.0, 503, 30, 1, 0, 0, 1, 586, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 12340),
 (181689, 15, 7546, 'Zeppelin, Horde (Cloudkisser)', '', '', '', 1.0, 737, 30, 1, 0, 0, 0, 610, 3481, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 12340),
 (186238, 15, 7546, 'Zeppelin, Horde (The Mighty Wind)', '', '', '', 1.0, 712, 30, 1, 0, 0, 0, 613, 3481, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 12340),
 (186371, 15, 3031, 'Zeppelin', '', '', '', 1.0, 727, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 'go_transport_westguard_zeppelin', 12340),
@@ -24,8 +22,6 @@ INSERT INTO acore_world.gameobject_template (entry, `type`, displayId, name, Ico
 
 -- gameobject_template_addon
 INSERT INTO acore_world.gameobject_template_addon (entry, faction, flags, mingold, maxgold, artkit0, artkit1, artkit2, artkit3) VALUES
-(177233, 0, 40, 0, 0, 0, 0, 0, 0),
-(181646, 0, 40, 0, 0, 0, 0, 0, 0),
 (181689, 0, 40, 0, 0, 0, 0, 0, 0),
 (186238, 0, 40, 0, 0, 0, 0, 0, 0),
 (186371, 0, 40, 0, 0, 0, 0, 0, 0),
